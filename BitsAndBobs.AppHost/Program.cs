e9833279e105670b118f5cb212e25900c8cc609f
marsupialmouse <16873013+marsupialmouse@users.noreply.github.com>
2025-03-26 22:54:57 +1100

    Add projects to Aspire

--- BitsAndBobs.AppHost/Program.cs
+++ BitsAndBobs.AppHost/Program.cs
@@ -2,7 +2,11 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var api = builder.AddProject<BitsAndBobs_Api>("api").WithExternalHttpEndpoints();
 
-builder.AddProject<BitsAndBobs_Api>("api");
+builder
+    .AddViteApp("ui", "../BitsAndBobs.UI", packageManager: "yarn")
+    .WithReference(api)
+    .WaitFor(api);
 
 builder.Build().Run();
